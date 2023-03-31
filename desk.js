7d1ecd82b9fb24e0aef75e73bb53c87184186007
santosh-vighnesh <90257274+santosh-vighnesh@users.noreply.github.com>
2023-03-31 21:06:17 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,58 @@
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('desk.jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw()
+ {
+  image(img, 0, 0, 640, 420);
+
+  if (status !="")
+   {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+    objectDetector.detect(img,gotResult);
+    for (i = 0; i < objects.length; i++) 
+    {
+
+  
+      document.getElementById("status").innerHTML = "Status : Objects Detected";
+      fill(r, g, b);
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+      noFill();
+      stroke(r, g, b);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
